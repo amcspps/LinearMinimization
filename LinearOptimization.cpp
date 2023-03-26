d1097b00562dcfd4c61bf903657aa09eddf4976c
kchnms <pashafizmat1@gmail.com>
2023-03-26 15:06:34 +0300

    initial commit

---
+++ LinearOptimization.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <cmath>
+#include <vector>
+using namespace std;
+//x in [1.5, 3.2]
+
+double f(double x) {
+	return exp(-x) * sin(2 * x);
+}
+double trial_point_method(double a, double b) {
+	vector<double> net;
+	for (int i = 1; i <= 3; i++) {
+		net.push_back(a + (b - a) / 4 * i);
+	}
+
+}
+int main() {
+	std::cout << "qwe" << std::endl;
+	return 0;
+}
